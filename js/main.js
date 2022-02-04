bd72c8d2ad70c85af6b653c63ffcaa08f154a6a9
paratrooper-94 <ermilov1994@yandex.ru>
2022-02-04 07:15:33 +0300

    - [x] Add animation to buttons (15+) - [x] Menu not working
    - [x] Добавить анимацию кнопкам (15+)
    - [x] Не работает меню

--- js/main.js
+++ js/main.js
@@ -466,97 +466,97 @@ let products = new ProductsList(cart);//Если мы хотим использ�
 
 
 
-// // ======================================================
+// ======================================================
 
 
 
 
-// // Меню бургер на JavaScript
+// Меню бургер на JavaScript
 
-// "use strict"
+"use strict"
 
-// const isMobile = {
-// 	Android: function () {
-// 		return navigator.userAgent.match(/Android/i);
-// 	},
-// 	BlackBerry: function () {
-// 		return navigator.userAgent.match(/BlackBerry/i);
-// 	},
-// 	iOS: function () {
-// 		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-// 	},
-// 	Opera: function () {
-// 		return navigator.userAgent.match(/Opera Mini/i);
-// 	},
-// 	Windows: function () {
-// 		return navigator.userAgent.match(/IEMobile/i);
-// 	},
-// 	any: function () {
-// 		return (
-// 			isMobile.Android() ||
-// 			isMobile.BlackBerry() ||
-// 			isMobile.iOS() ||
-// 			isMobile.Opera() ||
-// 			isMobile.Windows());
-// 	}
-// };
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
 
-// if (isMobile.any()) {
-// 	document.body.classList.add('_touch');
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
 
-// 	let menuArrows = document.querySelectorAll('.menu__arrow');
-// 	if (menuArrows.length > 0) {
-// 		for (let index = 0; index < menuArrows.length; index++) {
-// 			const menuArrow = menuArrows[index];
-// 			menuArrow.addEventListener("click", function (e) {
-// 				menuArrow.parentElement.classList.toggle('_active');
-// 			});
-// 		}
-// 	}
+	let menuArrows = document.querySelectorAll('.menu__arrow');
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function (e) {
+				menuArrow.parentElement.classList.toggle('_active');
+			});
+		}
+	}
 
-// } else {
-// 	document.body.classList.add('_pc');
-// }
+} else {
+	document.body.classList.add('_pc');
+}
 
-// // Меню бургер 
-// const iconMenu = document.querySelector('.menu__icon');
-// const menuBody = document.querySelector('.menu__body');
-// if (iconMenu) {
-// 	iconMenu.addEventListener("click", function (e) {
-// 		document.body.classList.toggle('_lock');
-// 		iconMenu.classList.toggle('_active');
-// 		menuBody.classList.toggle('_active');
-// 	});
-// }
-
-
-// // Прокрутка при клике
-// const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-// if (menuLinks.length > 0) {
-// 	menuLinks.forEach(menuLink => {
-// 		menuLink.addEventListener("click", onMenuLinkClick);
-// 	});
-
-// 	function onMenuLinkClick(e) {
-// 		const menuLink = e.target;
-// 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-// 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
-// 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-
-// 			if (iconMenu.classList.contains('_active')) {
-// 				document.body.classList.remove('_lock');
-// 				iconMenu.classList.remove('_active');
-// 				menuBody.classList.remove('_active');
-// 			}
-
-// 			window.scrollTo({
-// 				top: gotoBlockValue,
-// 				behavior: "smooth"
-// 			});
-// 			e.preventDefault();
-// 		}
-// 	}
-// }
+// Меню бургер 
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
 
 
-// // Меню бургер на JavaScript
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
+
+// Меню бургер на JavaScript
